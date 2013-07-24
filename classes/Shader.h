fa65c71de04f3b2b209c013b68ba09231bdba6d5
sangyoon <flexibles@naver.com>
2013-07-24 23:10:27 +0900

    Create Shader.h

---
+++ classes/Shader.h
@@ -0,0 +1,25 @@
+#ifndef SHADERMANAGER_HEADER_INCLUDED
+#define SHADERMANAGER_HEADER_INCLUDED
+
+#include <cassert>
+#include <iostream>
+#include <GL\glew.h>
+
+class Shader
+{
+private:
+    template< int N > GLuint compile( char const *( &source )[ N ] , GLuint shaderType );
+
+public:
+	template< int N , int M > Shader( GLchar const *( &vertextSource )[ N ] , GLchar const *( &fragmentSource )[ M ] );
+	~Shader( void );
+
+	operator GLuint();
+	void operator()( void );
+
+private:
+	GLuint shaderProgram;
+	GLuint vertexShader, fragmentShader;
+};
+
+#endif
